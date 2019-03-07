7f7ef5353ad7e36f8bd9821f57bdeebf6a741d20
Hantao Zhang <30546580+zht022@users.noreply.github.com>
2019-03-07 12:24:35 +0800

    Experiments Functions

--- sto_adv_BA_exp.py
+++ sto_adv_BA_exp.py
@@ -14,8 +14,11 @@ def Exp(alg, playtimes, c, other_alg_parameters, N, K, loss_generate, losses, mu
 
     Outputs:
     - error_rate.
+    - trigger_time.
     '''
     error = 0
+    trigger_time = 0
+    trigger_time_list = []
 
     if (alg == Successive_Halving):
         print('Begin Successive Halving: {0} budgets-------------------------------' \
@@ -60,22 +63,34 @@ def Exp(alg, playtimes, c, other_alg_parameters, N, K, loss_generate, losses, mu
     if (alg == S3_BA):
         print('Begin S3-BA: {0} budgets; exploration param c = {1}; confidence param delta = {2} -------------------------------' \
               .format(N,other_alg_parameters[1],other_alg_parameters[0]))
+        trigger_time = 0
+        
         if loss_generate == False:
             for play in np.arange(1, playtimes+1):
-                arm = S3_BA(other_alg_parameters, N, K, loss_generate, losses[play-1, :, :], mu, var, turn_bud_to_N)
+                arm, trigger, trigger_timing = S3_BA(other_alg_parameters, N, K, loss_generate, losses[play-1, :, :], mu, var, turn_bud_to_N)
                 if (arm != best_arm):
                     error += 1
                 if (verbose and play % verbose == 0):
                     print('{0} plays finished ; up to now, {1} errors ' \
                           .format(play, error))
+                if trigger:
+                    trigger_time += 1
+                    trigger_time_list.append(trigger_timing)
+                    
         else:
             for play in np.arange(1, playtimes+1):
-                arm = S3_BA(other_alg_parameters, N, K, loss_generate, [], mu, var, turn_bud_to_N)
+                arm, trigger, trigger_timing = S3_BA(other_alg_parameters, N, K, loss_generate, [], mu, var, turn_bud_to_N)
                 if (arm != best_arm):
                     error += 1
                 if (verbose and play % verbose == 0):
                     print('{0} plays finished ; up to now, {1} errors ' \
                           .format(play, error))
+                if trigger:
+                    trigger_time += 1
+                    trigger_time_list.append(trigger_timing)
         
-    return float(error / playtimes)
+    if len(trigger_time_list) == 0:
+        trigger_time_list.append(-1)
+        
+    return float(error * 1.0 / playtimes), float(trigger_time * 1.0 / playtimes), trigger_time_list
 
